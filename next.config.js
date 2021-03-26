572065c0caed6302a2b05b6e81e517b4103ef127
William Rijksen <william.rijksen@incentro.com>
2021-03-26 22:37:11 +0100

    drop cache-control for heavy call

--- next.config.js
+++ next.config.js
@@ -1,16 +1,2 @@
 module.exports = {
-    async headers() {
-        return [
-            {
-                source: '/heavy',
-                headers: [
-                    // disable cache for the k6 example
-                    // {
-                    //     key: 'Cache-Control',
-                    //     value: 's-maxage=60, stale-while-revalidate',
-                    // }
-                ],
-            },
-        ]
-    },
 }
